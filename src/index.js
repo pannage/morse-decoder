03ed5713e7b98f5e06e981ee00600c5bc5aeab99
Tatsiana <pannage@gmail.com>
2020-10-05 22:08:53 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let sentence = ''
+    let morseLetter = ''
+    for (let i = 0; i*10 < expr.length; i++) {
+        let word = expr.substring(i*10, i*10+10).replace(/0*/, '')
+        if (word === '**********') {
+            sentence  = sentence + ' '
+        } else {
+            for (let j =0; j+1 < word.length; j +=2) {
+                let codeLetter = '' + word[j] + word[j+1]
+                if (codeLetter === '10') {
+                    morseLetter = morseLetter + '.'
+                } else {
+                    morseLetter = morseLetter + '-'
+                }
+
+            }
+
+            sentence  = sentence + MORSE_TABLE[morseLetter]
+            morseLetter = ''
+        }
+    }
+
+
+    return sentence
+
 }
 
 module.exports = {
